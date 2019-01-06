d5418b4b31fef2b79af34a2d5dcbf1f6ae527158
coderaj <arjunmadhu044@gmail.com>
2019-01-06 13:28:00 +0530

    'success'

--- app/src/main/java/com/ihrd/arjun/sum/WelcomeActivity.java
+++ app/src/main/java/com/ihrd/arjun/sum/WelcomeActivity.java
@@ -22,24 +22,25 @@ public class WelcomeActivity extends AppCompatActivity {
         n3=(EditText)findViewById(R.id.n3);
         res=(EditText)findViewById(R.id.res);
         b=(Button)findViewById(R.id.b);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int num1=Integer.parseInt(n1.getText().toString());
-                int num2=Integer.parseInt(n2.getText().toString());
-                int num3=Integer.parseInt(n3.getText().toString());
-                if((num1>num2)&&(num1>num3)){
-                    large=num1;
+    b.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int num1=Integer.parseInt(n1.getText().toString());
+            int num2=Integer.parseInt(n2.getText().toString());
+            int num3=Integer.parseInt(n3.getText().toString());
+            if((num1>num2)&&(num1>num3)){
+                large=num1;
             }else if((num2>num1)&&(num2>num3)){
-                    large=num2;
-                }else if((num3>num1)&&(num3>num2)){
-                    large=num3;
-                }else{
-                    getRes="All equal";
-                }
-                res.setVisibility(View.VISIBLE);
-                res.setText(Integer.parseInt(large));
-        });
+                large=num2;
+            }else if((num3>num1)&&(num3>num2)){
+                large=num3;
+            }else{
+                getRes="All equal";
+            }
+            res.setVisibility(View.VISIBLE);
+            res.setText(String.valueOf(large));
+        }
+    });
+}
 
-    }
 }
